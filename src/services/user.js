2e6640504c4a928385fbeefc71dc970ece87477f
Luis Carlos <carlos@queo.com.co>
2021-08-25 21:39:09 -0500

    [FIX] url change

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const server = process.env.REACT_APP_URL_SERVER || 'https://dummyapi.io/data/api';
+const server = process.env.REACT_APP_URL_SERVER || 'https://dummyapi.io/data/v1/';
 const headers = {headers: { 'app-id': '606a72e16f9ea3adb4b5a8c5' }}
 
 export const getUser = async(userId) =>{

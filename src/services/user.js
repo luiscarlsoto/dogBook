6e934553d747408b235a748a37c7ef4725ff4e73
carlos <carlos@queo.com.co>
2022-06-01 17:54:30 -0400

    [REMOVE] env url

--- src/services/user.js
+++ src/services/user.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const server = process.env.REACT_APP_URL_SERVER || 'https://dummyapi.io/data/v1';
+const server = 'https://dummyapi.io/data/v1';
 const headers = {headers: { 'app-id': '61270899eaa192d74e01893b' }}
 
 export const getUser = async(userId) =>{
